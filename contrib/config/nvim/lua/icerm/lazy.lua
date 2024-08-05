b3555cc419d500de23a34181a7c8b49916e45391
icerm li <haohan@DESKTOP-6M5IRFV>
2024-08-05 22:26:25 +0800

    add auto and surround

--- contrib/config/nvim/lua/icerm/lazy.lua
+++ contrib/config/nvim/lua/icerm/lazy.lua
@@ -11,5 +11,13 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("icerm.plugins")
+require("lazy").setup("icerm.plugins", {
+    checker = {
+        enabled = true,
+        notify = false,
+    },
+    change_detection = {
+        notify = false,
+    },
+})
 
