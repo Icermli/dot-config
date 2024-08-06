f832fa782855de8f7efe9d6b32d5546f04b72b7a
icerm li <haohan@DESKTOP-6M5IRFV>
2024-08-06 20:59:29 +0800

    add airline

--- contrib/config/nvim/lua/icerm/plugins/init.lua
+++ contrib/config/nvim/lua/icerm/plugins/init.lua
@@ -1,4 +1,5 @@
 return {
     "nvim-lua/plenary.nvim", -- lua functions that many plugins use
     "christoomey/vim-tmux-navigator", -- tmux & split window navigation
+    { "bluz71/vim-nightfly-colors", name = "nightfly", lazy = false, priority = 1000 },
 }
